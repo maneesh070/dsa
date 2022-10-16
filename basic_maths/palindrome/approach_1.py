3984767820765700368076a768ca11f23435cefe
Maneesh Kumar <maneeshkumar070@gmail.com>
2022-10-16 11:07:36 +0530

    Palindrome number

---
+++ basic_maths/palindrome/approach_1.py
@@ -0,0 +1,18 @@
+import re
+from tkinter import N
+
+
+def main():
+    n = 3432
+    x = n
+    reverse = 0
+    while x != 0:
+        reverse = reverse*10 + x%10
+        x = x//10
+    if n == reverse:
+        print(f"The number {n} is a palindrome")
+    else:
+        print(f"The number {n} is not a palindrome")
+
+if __name__ == '__main__':
+    main()
