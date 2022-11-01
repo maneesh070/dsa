379422816b0ffd96db2de083bb6bf5bd072e2106
Maneesh Kumar <maneeshkumar070@gmail.com>
2022-11-01 17:31:23 +0530

    LCM method added

--- basic_maths/gcd/approach_1.py
+++ basic_maths/gcd/approach_1.py
@@ -1,8 +1,9 @@
 def hcf(A, B):
+    x = []
     if A == 0:
-        return B
+        x.append(B)
     elif B == 0:
-        return A
+        x.append(A)
     elif A != 0 and B != 0:
         if A > B:
             factor = B - 1
@@ -22,12 +23,29 @@ def hcf(A, B):
                     factor -= 1
                 gcd = factor
 
+        x.append(gcd)
 
-        return gcd
+    if A>B:
+        if A%B == 0:
+            x.append(A)
+        else:
+            M = A
+            while M%B != 0 or M%A != 0:
+                M += 1
+            x.append(M)
+    else:
+        if B%A == 0:
+            x.append(B)
+        else:
+            N = B
+            while N%A != 0 or N%B != 0:
+                N += 1
+            x.append(N)
+
+    return x[::]
 
 
-
-n1 = 0
+n1 = 20
 n2 = 15
 gcd = hcf(n1, n2)
 print(gcd)
