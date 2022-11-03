0113c50cb34a9fe27a31f109b017131f1e36ec4b
Maneesh Kumar <maneeshkumar070@gmail.com>
2022-11-03 18:50:57 +0530

    Simplified previous gcd and lcm method

--- basic_maths/gcd/approach_1.py
+++ basic_maths/gcd/approach_1.py
@@ -1,51 +1,37 @@
 def hcf(A, B):
     x = []
     if A == 0:
-        x.append(B)
+        gcd = B
     elif B == 0:
-        x.append(A)
+        gcd = A
     elif A != 0 and B != 0:
         if A > B:
-            factor = B - 1
-            if A%B == 0:
-                gcd = B
-            else:
-                while A%factor != 0 or B%factor != 0:
-                    factor -= 1
-                gcd = factor
+            factor = B
+            while A%factor != 0 or B%factor != 0:
+                factor -= 1
+            gcd = factor
 
         else:
-            factor = A - 1
-            if B%A == 0:
-                gcd = A
-            else:
-                while B%factor != 0 or A%factor != 0:
-                    factor -= 1
-                gcd = factor
-
-        x.append(gcd)
+            factor = A
+            while B%factor != 0 or A%factor != 0:
+                factor -= 1
+            gcd = factor
 
     if A>B:
-        if A%B == 0:
-            x.append(A)
-        else:
-            M = A
-            while M%B != 0 or M%A != 0:
-                M += 1
-            x.append(M)
+        M = A
+        while M%B != 0 or M%A != 0:
+            M += 1
+        lcm = M
     else:
-        if B%A == 0:
-            x.append(B)
-        else:
-            N = B
-            while N%A != 0 or N%B != 0:
-                N += 1
-            x.append(N)
+        N = B
+        while N%A != 0 or N%B != 0:
+            N += 1
+        lcm = N
 
-    return x[::]
+    return gcd, lcm
 
 
-n1 = 20
+n1 = 24
 n2 = 15
 gcd = hcf(n1, n2)
 print(gcd)
