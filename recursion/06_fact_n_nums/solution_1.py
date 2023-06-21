c6935a6dd11bd40c37f24171867efb704e668da0
Maneesh Kumar <maneeshkumar070@gmail.com>
2023-06-21 20:47:17 +0530

    Result returned as array

--- recursion/06_fact_n_nums/solution_1.py
+++ recursion/06_fact_n_nums/solution_1.py
@@ -1,14 +1,16 @@
 class Solution():
-    def factorial_numbers(self, n, count, i):
+    def factorial_numbers(self, n, count, i, arr):
         if i > n:
             return
-        print(i, end=" ")
-        self.factorial_numbers(n, count+1, i*(count+1))
+        # print(i, end=" ")
+        arr.append(i)
+        self.factorial_numbers(n, count+1, i*(count+1), arr)
+        return arr
 
 
 if __name__ == "__main__":
     n = 24
     1 # This is a expression whose value is 1 after evaluating this expression. Purpose: "Learning"
     ob = Solution()
-    ob.factorial_numbers(n, 1, 1)
-    print()
+    print(ob.factorial_numbers(n, 1, 1, []))
+    # print()
